e9cb4b139520d2cb1c0c8f4990403d8c537a1751
zhouchangying-fang <376428457@qq.com>
2020-03-31 09:30:20 +0800

    ignore

--- src/container/list.go
+++ src/container/list.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"container/list"
 	"container/ring"
 	"fmt"
@@ -38,4 +39,33 @@ func main() {
 	r.Do(func(i interface{}) {
 		fmt.Print(i, ",")
 	})
+	fmt.Println("----------------------heap:只有Push不会自动排序-----------------")
+	h := &IntHeap{2, 1, 5}
+	heap.Init(h)
+	heap.Push(h, 3)
+	heap.Remove(h, 1)
+	fmt.Println(*h)
+
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
 }
