a5ed1a8bc4d33958c0f0f4ad6c62f688d7f53f95
heini <376428457@qq.com>
2020-03-31 08:02:59 +0800

    heini

--- src/database/database.go
+++ src/database/database.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	ctx context.Context
 )
 
 func main() {
-	fmt.Printf("%s", sql.Drivers())
+	fmt.Printf("Drivers:---%s\n", sql.Drivers())
+	db, err := sql.Open("mysql", "root:1@tcp(127.0.0.1:3306)/golang?charset=utf8")
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+	err1 := db.Ping()
+	if err1 != nil {
+		fmt.Println("err1:", err1)
+		return
+	}
+	defer db.Close()
+	fmt.Println("Driver:---", db.Driver())
+
 }
