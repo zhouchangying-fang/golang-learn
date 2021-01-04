00639574ade8962c6bb9b59d0ba9f5a58a086fe2
zhouchangying-fang <376428457@qq.com>
2021-01-04 11:50:42 +0800

    6666

--- day1221/d01/service/endpoint.go
+++ day1221/d01/service/endpoint.go
@@ -4,47 +4,42 @@ import (
 	"context"
 	"errors"
 	"github.com/go-kit/kit/endpoint"
-	kithttp "github.com/go-kit/kit/transport/http"
-	"github.com/gorilla/mux"
-	"net/http"
 	"strings"
 )
 
-func MakeArithmeticEndpoint(svc Service) endpoint.Endpoint {
+type ServiceImplRequest struct {
+	RequestType string `json:"request_type"`
+	A           int    `json:"a"`
+	B           int    `json:"b"`
+}
+type ServiceImplResponse struct {
+	Result int   `json:"result"`
+	Error  error `json:"error"`
+}
+
+func MakeServiceImplEndpoint(svc Service) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
-		req := request.(ArithmeticRequest)
+		req := request.(ServiceImplRequest)
 		var (
-			res, a, b int
-			calError  error
+			res  int
+			a    = req.A
+			b    = req.B
+			errs error
 		)
-		a = req.A
-		b = req.B
 		if strings.EqualFold(req.RequestType, "Add") {
 			res = svc.Add(a, b)
-		} else if strings.EqualFold(req.RequestType, "Substract") {
+		} else if strings.EqualFold(req.RequestType, "Subtract") {
 			res = svc.Subtract(a, b)
 		} else if strings.EqualFold(req.RequestType, "Multiply") {
 			res = svc.Multiply(a, b)
 		} else if strings.EqualFold(req.RequestType, "Divide") {
-			res, calError = svc.Divide(a, b)
+			res, errs = svc.Divide(a, b)
 		} else {
-			return nil, errors.New("不知道的请求类型")
+			return nil, errors.New("not know request type")
 		}
-		return ArithmeticResponse{
-			res,
-			calError,
+		return ServiceImplResponse{
+			Result: res,
+			Error:  errs,
 		}, nil
 	}
-}
-
-func MakeHttpHandler(ctx context.Context, endpoint endpoint.Endpoint) http.Handler {
-	r := mux.NewRouter()
-	r.Methods("POST").Path("/calculate/{type}/{a}/{b}").Handler(
-		kithttp.NewServer(
-			endpoint,
-			decodeArithmeticRequest,
-			encodeArithmeticResponse,
-		),
-	)
-	return r
 }
